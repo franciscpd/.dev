d5703ec988a5965d3cae3b335b882aa635d860ef
Francisross Soares de Oliveira <franciscpd@gmail.com>
2019-09-06 22:05:36 -0300

    Add profile component

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Francisross Soares de Oliveira</h1>
+    <h2>Solution Developer</h2>
+    <p>Collaborator 0soft, web and mobile innovation company</p>
+  </div>
+)
+
+export default Profile
