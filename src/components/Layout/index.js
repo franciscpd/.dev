95562eb0640bb0b5983f23d518efd2584b734fba
Francisross Soares de Oliveira <franciscpd@gmail.com>
2019-09-09 22:59:58 -0300

    Styles and functional components

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,16 +1,17 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-import Profile from "./Profile"
+import * as S from "./styles"
+import GlobalStyles from "../../styles/global"
+import Sidebar from "../Sidebar"
 
 const Layout = ({ children }) => {
   return (
-    <>
-      <aside>
-        <Profile />
-      </aside>
-      <main>{children}</main>
-    </>
+    <S.LayoutWrapper>
+      <GlobalStyles />
+      <Sidebar />
+      <S.LayoutMain>{children}</S.LayoutMain>
+    </S.LayoutWrapper>
   )
 }
 
