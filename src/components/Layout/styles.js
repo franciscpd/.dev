1be78b8584d1fae4dd98d2a896e463091665f4c7
Francisross Soares de Oliveira <franciscpd@gmail.com>
2019-09-11 20:15:43 -0300

    Add component PostItem

--- src/components/Layout/styles.js
+++ src/components/Layout/styles.js
@@ -6,6 +6,6 @@ export const LayoutWrapper = styled.section`
 
 export const LayoutMain = styled.main`
   min-height: 100vh;
-  padding: 0 3.75rem 0 20 rem;
+  padding: 0 3.75rem 0 20rem;
   width: 100%;
 `
---
+++ src/components/PostItem/index.js
@@ -0,0 +1,41 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+import * as S from "./styles"
+
+const PostItem = ({
+  slug,
+  category,
+  background,
+  date,
+  timeToRead,
+  title,
+  description,
+}) => {
+  return (
+    <S.PostItemLink to={`/${slug}`}>
+      <S.PostItemWrapper>
+        <S.PostItemTag background={background}>{category}</S.PostItemTag>
+        <S.PostItemInfo>
+          <S.PostItemData>
+            {date} • {timeToRead} min de leitura
+          </S.PostItemData>
+          <S.PostItemTitle>{title}</S.PostItemTitle>
+          <S.PostItemDescription>{description}</S.PostItemDescription>
+        </S.PostItemInfo>
+      </S.PostItemWrapper>
+    </S.PostItemLink>
+  )
+}
+
+PostItem.propTypes = {
+  slug: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+  background: PropTypes.string,
+  date: PropTypes.string.isRequired,
+  timeToRead: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+}
+
+export default PostItem
